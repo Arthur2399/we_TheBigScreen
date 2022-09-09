7a36d7d24d27fd3e72b5fdb14bee4bc21e0324dd
Arthur Chavez <arthurchavez2399@gmail.com>
2022-09-09 10:33:01 -0500

    [ADD] Sistemas de archivos - Layout menu

---
+++ src/private/layout/Menu.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { Options } from './components/Options';
+import './Menu.css';
+
+export const Menu = () => {
+    const [toggle, setToggle] = useState(false);
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+
+    return (
+        <nav>
+            <div className="navbar-left">
+                <a href="/" className="nav-logo"><img src='' alt=" logo" /></a>
+                <ul>
+                    <li>
+                        <a href="employes">Empleados</a>
+                    </li>
+                    <li>
+                        <a href="movies">Películas</a>
+                    </li>
+                    <li>
+                        <a href="survey">Encuestas</a>
+                    </li>
+                    <li>
+                        <a href="awards">Premios</a>
+                    </li>
+                    <li>
+                        <a href="accredit">Créditos</a>
+                    </li>
+                    <li>
+                        <a href="exchange">Canjear</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <ul>
+                    <li className="navBar-email" onClick={handleToggle}>
+                        Arthur Chavez
+                    </li>
+                    <li className="navBar-user" onClick={handleToggle}>
+                        <img src='' alt="User" />
+                    </li>
+                </ul>
+            </div>
+            {toggle &&<Options/>}
+        </nav>
+    )
+}
---
+++ src/private/layout/components/Options.jsx
@@ -0,0 +1,18 @@
+import './Options.css';
+export const Options = () => {
+    return (
+        <div className="desktop-menu">
+            <ul>
+                <li>
+                    <a href="/">Mi perfil</a>
+                </li>
+                <li>
+                    <a href="/">Cambiar contraseña</a>
+                </li>
+                <li>
+                    <a href="/" className="">Cerrar Sesion</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
