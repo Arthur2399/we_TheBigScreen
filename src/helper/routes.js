c6d86f64c360c07c5f15bd2bda6ff1652f0fb0d6
Arthur Chavez <arthurchavez2399@gmail.com>
2022-09-22 20:39:42 -0500

    [ADD] index hooks auth

--- src/helper/routes.js
+++ src/helper/routes.js
@@ -1,6 +1,8 @@
 export const API_REST       = '127.0.0.1:8087'
-export const API_BILLBOARD  = `http://${API_REST}/movies/billboard`;
-export const API_LOGIN      = `http://${API_REST}/user/api/token/`;
-export const API_EMPLOYED   = `http://${API_REST}/user/employee`;
-export const API_MENU       = `http://${API_REST}/menu/menu`;
-export const API_WHATSAPP   = 'https://api.whatsapp.com/send?phone=0997106357&text=¡Hola!%20Necesito%20ayuda%20con%20mi%20con%20contraseña%20de%20The%20Big%20Screen.'
+
+export const API_BILLBOARD       = `http://${API_REST}/movies/billboard`;
+export const API_LOGIN           = `http://${API_REST}/user/api/token/`;
+export const API_EMPLOYED_LIST   = `http://${API_REST}/user/liste`;
+export const API_MENU            = `http://${API_REST}/menu/menu`;
+
+export const API_WHATSAPP        = 'https://api.whatsapp.com/send?phone=0997106357&text=¡Hola!%20Necesito%20ayuda%20con%20mi%20con%20contraseña%20de%20The%20Big%20Screen.'
