39c823f10c31858f80f7b18e1e6a7bf2968c551e
Arthur Chavez <arthurchavez2399@gmail.com>
2022-09-25 18:30:15 -0500

    [UPDATE] Exchange

--- src/components/ClientProfile.jsx
+++ src/components/ClientProfile.jsx
@@ -4,7 +4,7 @@ export const ClientProfile = () => {
   return (
     <div className="user-profile-info">
     <div className="user-profile-info-photo">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Zac_Efron%2C_Eva_Rinaldi_Photography_%2810%29_%2834732926735%29.jpg/800px-Zac_Efron%2C_Eva_Rinaldi_Photography_%2810%29_%2834732926735%29.jpg" alt="user" />
+        <img src="/assets/img/Premios.png" alt="user" />
         <h2>Zack Efron</h2>
     </div>
     <div className="user-profile-info-points">
