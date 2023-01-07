f09d491721f409189124e5402a57f106b8165268
Arthur Chavez <arthurchavez2399@gmail.com>
2023-01-07 17:07:38 -0500

    [ADD] Timer

---
+++ src/private/game/hooks/usePostRoom.js
@@ -0,0 +1,19 @@
+export const usePostRoom = async (employedata) => {
+    const bearer = JSON.parse(localStorage.getItem('jwt'));
+    
+    const options = {
+        method: 'POST',
+        headers: {
+           'Content-Type': 'application/json',
+           Authorization: 'Bearer '+ bearer.sessiontoken,
+        },
+        body: employedata
+    };
+    const response = await fetch(`${API_EMPLOYED_CREATE}`, options)
+    if(response.status ==400){
+        const bad= await response.json(); 
+        alert(JSON.stringify(bad.email[0]));
+    }else{
+      window.location.reload(true);
+    }
+}
--- src/private/game/pages/LobbyGame.jsx
+++ src/private/game/pages/LobbyGame.jsx
@@ -3,72 +3,64 @@ import logotbs from '/assets/logos/tbs_logo.svg'
 import qr from '/assets/logos/qr.svg'
 import apple from '/assets/icons/Apple.svg'
 import google from '/assets/icons/Google.png'
+import { useEffect, useState } from 'react';
 
 export const LobbyGame = () => {
-  return (
-    <section className="GameLobby">
-        <img id="logo" src={logotbs} alt=""/>
-        <div className="GameLobby-container">
-            <div className="GameLobby-container-qr">
-                <span>Escanea el codigo QR</span>
-                <div className=" GameLobby-qr-background">
-                    <img src={qr} alt=""/>
+
+    const [timer, setTimer] = useState("")
+
+    useEffect(() => {
+        function countdown(minutes, seconds) {
+            var time = minutes * 60 + seconds;
+            var interval = setInterval(function () {
+                if (time == 0) {
+                    setTimeout(function () {
+                        console.log('termino...');
+                    }, 10);
+                    clearInterval(interval);
+                    //HACER ALGO
+                }
+                var minutes = Math.floor(time / 60);
+                if (minutes < 10) minutes = "0" + minutes;
+                var seconds = time % 60;
+                if (seconds < 10) seconds = "0" + seconds;
+                var text = minutes + ':' + seconds;
+                setTimer(text)
+                time--;
+            }, 1000);
+        }
+        countdown(3, 0);
+    }, [])
+
+    return (
+        <section className="GameLobby">
+            <img id="logo" src={logotbs} alt="" />
+            <div className="GameLobby-container">
+                <div className="GameLobby-container-qr">
+                    <span>Escanea el codigo QR</span>
+                    <div className=" GameLobby-qr-background">
+                        <img src={qr} alt="" />
+                    </div>
+                    <h1>{timer}</h1>
                 </div>
-                <h1>3:00</h1>
-            </div>
-            <div className="GameLobby-container-player">
-                <h1>Jugadores</h1>
-                <div className="GameLobby-container-players">
-                    <span>Arthur Chavez</span>
-                    <span>Miguel Pino</span>
-                    <span>Ruben Salas</span>
-                    <span>Efrain Raza</span>
-                    <span>Fausto Andino</span>
-                    <span>Marco Perez</span>
-                    <span>Sofia Ruales</span>
-                    <span>Mateo Lopez</span>
-                    <span>Julian Tapia</span>
-                    <span>Josua Santana</span>
-                    <span>Paul Blacio</span>
-                    <span>Milton Garrido</span>
-                    <span>Luis Tapia</span>
-                    <span>Maria Pino</span>
-                    <span>Jessenia Raza</span>
-                    <span>Valeria Chavez</span>
-                    <span>Kathy Andrade</span>
-                    <span>Simon Bolivar</span>
-                    <span>Alex Calderon</span>
-                    <span>Arthur Chavez</span>
-                    <span>Miguel Pino</span>
-                    <span>Ruben Salas</span>
-                    <span>Efrain Raza</span>
-                    <span>Fausto Andino</span>
-                    <span>Marco Perez</span>
-                    <span>Sofia Ruales</span>
-                    <span>Mateo Lopez</span>
-                    <span>Julian Tapia</span>
-                    <span>Josua Santana</span>
-                    <span>Paul Blacio</span>
-                    <span>Milton Garrido</span>
-                    <span>Luis Tapia</span>
-                    <span>Maria Pino</span>
-                    <span>Jessenia Raza</span>
-                    <span>Valeria Chavez</span>
-                    <span>Kathy Andrade</span>
-                    <span>Simon Bolivar</span>
-                    <span>Alex Calderon</span>
+                <div className="GameLobby-container-player">
+                    <h1>Jugadores</h1>
+                    <div className="GameLobby-container-players">
+                        <span>Arthur Chavez</span>
+                        <span>Miguel Pino</span>
+                        <span>Fausto Andino</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="GameLobby-footer">
-            <h1>¿Aun no tienes la app oficial?</h1>
-            <h2>Descargala gratis</h2>
-            <h2>Buscala como The Big Screen</h2>
-            <div className="GameLobby-footer-stores">
-                <img src={apple} alt=""/>
-                <img src={google} alt=""/>
+            <div className="GameLobby-footer">
+                <h1>¿Aun no tienes la app oficial?</h1>
+                <h2>Descargala gratis</h2>
+                <h2>Buscala como The Big Screen</h2>
+                <div className="GameLobby-footer-stores">
+                    <img src={apple} alt="" />
+                    <img src={google} alt="" />
+                </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
