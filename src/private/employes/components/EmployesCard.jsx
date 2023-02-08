26419f1646ce519351b50b80511bb581499a6cfb
Sthefany <s_thef12@hotmail.es>
2023-02-07 20:29:17 -0500

    [ADD] Changes

--- src/private/employes/components/EmployesCard.jsx
+++ src/private/employes/components/EmployesCard.jsx
@@ -1,10 +1,11 @@
 import { API_IMAGE} from '../../../helper/routes';
 import './EmployesCard.css';
 
-export const EmployesCard = ({id,first_name, last_name,rol,image,openModal,setEmployeId}) => {
+export const EmployesCard = ({id,first_name, last_name,rol,image,openModal,setEmployeId, is_active}) => {
     return (
         <div className="employ-cards animate__animated animate__fadeIn" onClick={()=>{openModal(); setEmployeId(id)}} >
             <img src={`${API_IMAGE}${image}`} alt="employes" />
+            <p>{is_active.toString()}</p>
             <p>{first_name}</p>
             <p>{last_name}</p>
             <p>{rol}</p>
