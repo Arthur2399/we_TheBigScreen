54ce08ca0661b09c11cc7a736ab57b062ef9205d
Arthur Chavez <arthurchavez2399@gmail.com>
2023-03-14 08:48:57 -0500

    [ADD] Changes

--- src/private/game/pages/LobbyGame.jsx
+++ src/private/game/pages/LobbyGame.jsx
@@ -58,7 +58,7 @@ export const LobbyGame = () => {
                     time--;
                 }, 1000);
             }
-            countdown(0, 10);
+            countdown(3, 0);
 
             var totalTime = 5;
             const updateClock = () => {
